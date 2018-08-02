acc997697da1ee06040ec7c193a4d204827af1a2
Marius BALTEANU <marius.balteanu@zitec.com>
2018-08-02 13:04:51 +0000

    sync user quota

--- lib/ldap_sync/infectors/person.rb
+++ lib/ldap_sync/infectors/person.rb
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Redmine LDAP Sync.  If not, see <http://www.gnu.org/licenses/>.
 module LdapSync::Infectors::Person
-  ::Person::STANDARD_FIELDS = %w( phone job_title department_id )
+  ::Person::STANDARD_FIELDS = %w( phone job_title department_id quota )
 
   module InstanceMethods
 
